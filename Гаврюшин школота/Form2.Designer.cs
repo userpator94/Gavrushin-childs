790b8538f9b6c5c429e03e73bfa1d8d7e50c1c5a
Ilya Martynenko <36264807+userpator94@users.noreply.github.com>
2018-03-22 18:08:49 +0300

    Add files via upload
    Теперь всегда выводится ОДИН низкий и ОДИН высокий
    но рост низкого выводится иногда криво

--- Гаврюшин школота/Form2.Designer.cs
+++ Гаврюшин школота/Form2.Designer.cs
@@ -28,11 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -49,6 +76,18 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17"});
             this.comboBox2.Location = new System.Drawing.Point(206, 12);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(72, 21);
@@ -79,14 +118,179 @@
             this.button1.Size = new System.Drawing.Size(83, 21);
             this.button1.TabIndex = 4;
             this.button1.Text = "Показать";
+            this.toolTip1.SetToolTip(this.button1, "Построение нормативов может занять некоторое время");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column11,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column12,
+            this.Column9,
+            this.Column10});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(536, 70);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Параметр";
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 64;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "Nx";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 43;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Mx";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 42;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "mx";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 32;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "σх";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 42;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "P25";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 43;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "P50";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 42;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "P75";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 42;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Vx";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 40;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "r";
+            this.Column12.Name = "Column12";
+            this.Column12.Width = 33;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Rx/y";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 33;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "δR";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 33;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 147);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(502, 388);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // Column13
+            // 
+            this.Column13.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column13.HeaderText = "Оценка роста";
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            this.Column13.Width = 102;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Рост, см";
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            this.Column14.Width = 50;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "М-δR";
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            this.Column15.Width = 50;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Мср.";
+            this.Column16.Name = "Column16";
+            this.Column16.ReadOnly = true;
+            this.Column16.Width = 55;
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "М+δR";
+            this.Column17.Name = "Column17";
+            this.Column17.ReadOnly = true;
+            this.Column17.Width = 50;
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "М+2δR";
+            this.Column18.Name = "Column18";
+            this.Column18.ReadOnly = true;
+            this.Column18.Width = 50;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 532);
+            this.ClientSize = new System.Drawing.Size(561, 549);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -94,6 +298,8 @@
             this.Controls.Add(this.comboBox1);
             this.Name = "Form2";
             this.Text = "Нормативы";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,5 +312,26 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- Гаврюшин школота/Form2.cs
+++ Гаврюшин школота/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using Microsoft.Office.Interop.Excel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,244 @@ namespace Гаврюшин_школота
 {
     public partial class Form2 : Form
     {
+        public double stDevH = 0, stDevM = 0;
+        public double averageH = 0, averageM =0 , Rxy = 0, sigmaR = 0;        
+
         public Form2()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex == -1 || comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Проверьте выбраны ли ПОЛ и ВОЗРАСТ",
+                    "Внимание!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                return;
+            }
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
 
+            int page = 0;
+            if (comboBox1.SelectedIndex == 0) page += 11; //мужской пол с 12 страницы
+            page += Convert.ToInt32(comboBox2.SelectedItem.ToString()) - 6;
+
+            Microsoft.Office.Interop.Excel.Application excApp =
+                new Microsoft.Office.Interop.Excel.Application();
+            excApp.Visible = false;
+            Workbook wb;
+            Worksheet wsh;
+            string exeDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            exeDir = System.IO.Path.Combine(exeDir, "base.xls");
+            wb = excApp.Workbooks.Open(exeDir, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            wsh = (Worksheet)wb.Sheets[page];
+            var lastCell = wsh.Cells.SpecialCells(XlCellType.xlCellTypeLastCell);
+            if (lastCell.Row < 3)
+            {
+                MessageBox.Show("Данных в это категории недостаточно, чтобы построить нормативы");
+                return;
+            }
+            string[,] list = new string[lastCell.Row, 3]; // массив значений с листа равен по размеру листу
+            for (int i = 0; i < lastCell.Row; i++) //по всем колонкам
+                for (int j = 0; j < 3; j++) // по всем строкам кроме последней
+                    list[i, j] = wsh.Cells[i+ 1, j + 1].Text.ToString();//считываем текст в строку
+
+            int Nx = list.GetLength(0);
+            if (list[list.GetLength(0)-1, 1] == list[list.GetLength(0) - 2, 1] &&
+                list[list.GetLength(0)-1, 2] == list[list.GetLength(0) - 2, 2])
+                Nx = list.GetLength(0) - 1;
+
+            if (Nx < 100 && Nx > 2) MessageBox.Show(
+                "Записей о детях данной группы меньше 100",
+                "Обратите внимание",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+            if (Nx <= 2)
+            {
+                MessageBox.Show(
+                "Записей о детях данной группы меньше 2",
+                "Построение стандартов прервано",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Hand,
+                MessageBoxDefaultButton.Button1);
+                return;
+            }
+       
+            //это всё ради r
+            var wshFunc = excApp.WorksheetFunction;
+            double[] forCorrelation = new double[Nx];
+            double[] forPercent = new double[Nx];
+            for (int j = 0; j < Nx; j++)
+            {
+                forCorrelation[j] = double.Parse(list[j, 2]);
+                forPercent[j] = double.Parse(list[j, 1]);
+            }                
+            double r = wshFunc.Correl(forPercent, forCorrelation);
+            
+            //вывод в первую таблицу
+            dataGridDisplay1(1, list, Nx, r);            
+            dataGridDisplay1(2, list, Nx, r);
+
+            //вывод во вторую таблицу
+            dataGridDisplay2(Nx, list, Rxy);
+
+            //this.dataGridView1.Rows.Insert(0, "one", "two", "three", "four");
+            //DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[-1].Clone();
+            //row.Cells[0].Value = "Рост";
+            //row.Cells[1].Value = list.GetLength(0);
+            //row.Cells[2].Value = sumH/Nx;
+            //dataGridView1.Rows.Add(row);
+
+            wb.Close(false, Type.Missing, Type.Missing);
+            excApp.Quit();
+            GC.Collect();
+
+        }
+
+        public void dataGridDisplay1(int P, string[,] list, int N, double r)
+        {
+            double sum = 0;            
+            double sumOfDifference = 0;
+            for (int j = 0; j < N; j++)
+                sum += double.Parse(list[j, P]);
+
+            double[] forPercent = new double[N];
+            for (int j = 0; j < N; j++)
+            {
+                //делаем одномерный массив
+                forPercent[j] = double.Parse(list[j, P]);
+                //сумма квадратов разниц
+                sumOfDifference += Math.Pow(((double.Parse(list[j, P])) - sum / N), 2);
+            }
+            double stDeviation = Math.Sqrt(sumOfDifference / N); //N-1
+            if (P == 1)
+                stDevH = stDeviation;
+            if (P == 2)
+            {
+                stDevM = stDeviation;
+                Rxy = r * stDevM / stDevH;                
+                sigmaR = stDevM * Math.Sqrt(1 - r * r);                
+            }
+
+            double average = sum / N;
+            if (P == 1) averageH = average;
+            if (P == 2) averageM = average;            
+            double mediana = stDeviation / Math.Sqrt(N);            
+            double percent25 = Math.Round(Percentile(forPercent, 0.25), 1); //25%
+            double percent50 = Math.Round(Percentile(forPercent, 0.5), 1); //50%
+            double percent75 = Math.Round(Percentile(forPercent, 0.75), 1); //75%
+            double V = stDeviation / average * 100;
+
+            stDeviation = Math.Round(stDeviation, 1);
+            average = Math.Round((average), 1);
+            mediana = Math.Round((mediana), 1);
+            V = Math.Round(V, 1);
+            if (P == 1)
+                this.dataGridView1.Rows.Add
+                ("Рост", list.GetLength(0), average, mediana, stDeviation, percent25, percent50,
+                percent75, V, "-", "-", "-");
+            if (P == 2)
+                this.dataGridView1.Rows.Add
+                ("Масса", list.GetLength(0), average, mediana, stDeviation, percent25, percent50,
+                percent75, V, Math.Round(r, 1), Math.Round(Rxy, 1), Math.Round(sigmaR, 1));
+        }
+
+        public void dataGridDisplay2(int N, string[,] list, double Rxy)
+        {
+            //int Max = list
+            double[] forHeight = new double[N];
+            for (int i = 0; i < N; i++)
+                forHeight[i] = Math.Round(double.Parse(list[i, 1]));
+
+            double MaxHdouble = forHeight.Max();
+            int MaxH = Convert.ToInt32(MaxHdouble);
+            double MinHdouble = forHeight.Min();
+            int MinH = Convert.ToInt32(MinHdouble);
+            double[] forOutput = new double[MaxH - MinH+1];
+            for (int i = 0; i <= MaxH - MinH; i++)
+                forOutput[i] = i + MinH;
+
+            averageH = Math.Round(averageH);
+            averageM = Math.Round(averageM);
+            stDevH = Math.Round(stDevH);
+
+            string s = null;
+            double MsigmaR = 0;
+            int low = 0, high = 0;   
+            for (int i = 0; i < forOutput.Length; i++)
+            {                
+                if (forOutput[i] < Math.Ceiling(averageH - 2 * stDevH))
+                {
+                    if (low > 0) continue;
+                    else
+                    {
+                        s = "низкий  М-2,1δ и меньше";
+                        low++;
+                    }                                        
+                }                    
+                if (forOutput[i] < Math.Ceiling(averageH + stDevH) && 
+                    forOutput[i] >= Math.Ceiling(averageH - 2 * stDevH))
+                {
+                    if (s == null) goto AddLowValue;
+                    else s = "ниже среднего от М-1,1δ до М-2δ";
+                }                    
+                if (forOutput[i] < Math.Ceiling(averageH + stDevH) && 
+                    forOutput[i] >= Math.Ceiling(averageH - stDevH))
+                    s = "средний М±1δ";
+                if (forOutput[i] > Math.Ceiling(averageH + stDevH) && 
+                    forOutput[i] <= Math.Ceiling(averageH + 2 * stDevH))
+                    s = "выше среднего от М+1,1δ до М+2δ";
+                if (forOutput[i] > Math.Ceiling(averageH + 2 * stDevH))
+                {
+                    if (high > 0) continue;
+                    else
+                    {
+                        s = "высокий от М+2,1δ и больше";
+                        high++;
+                    }
+                }                    
+
+                MsigmaR = averageM + Rxy * (forOutput[i] - averageH);
+                
+                this.dataGridView2.Rows.Add
+                    (s, forOutput[i], Math.Round(MsigmaR - sigmaR, 1), Math.Round(MsigmaR, 1),
+                    Math.Round(MsigmaR + sigmaR, 1), Math.Round(MsigmaR + 2 * sigmaR,1));
+                continue;
+
+                AddLowValue:
+                {
+                    s = "низкий  М-2,1δ и меньше";
+                    MsigmaR = averageM + Rxy * (Math.Floor(averageH - 2.1 * stDevH) - averageH);
+                    this.dataGridView2.Rows.Add
+                    (s, forOutput[i]-1, Math.Round(MsigmaR - sigmaR, 1), Math.Round(MsigmaR, 1),
+                    Math.Round(MsigmaR + sigmaR, 1), Math.Round(MsigmaR + 2 * sigmaR, 1));
+                    i--;
+                }
+                             
+            }
+        }
+        public double Percentile(double[] sequence, double excelPercentile)
+        {
+            Array.Sort(sequence);
+            int N = sequence.Length;
+            double n = (N - 1) * excelPercentile + 1;
+            // Another method: double n = (N + 1) * excelPercentile;
+            if (n == 1d) return sequence[0];
+            else if (n == N) return sequence[N - 1];
+            else
+            {
+                int k = (int)n;
+                double d = n - k;
+                return sequence[k - 1] + d * (sequence[k] - sequence[k - 1]);
+            }
         }
     }
 }
